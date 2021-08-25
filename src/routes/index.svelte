f53a3cc3242a9b615b1e2a5a783a47637039fe24
Ryan LaBouve <imryanlabouve@gmail.com>
2021-08-25 13:59:47 -0500

    works poorly

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+let veryNormalText;
+let veryNormalTextRaw='';
+
+let charMap = {
+  "a": ["a"],
+  "b": ["b"],
+  "c": ["c"],
+  "d": ["d"],
+  "e": ["e"],
+  "f": ["f"],
+  "g": ["g"],
+  "h": ["h"],
+  "i": ["i"],
+  "j": ["j"],
+  "k": ["k"],
+  "l": ["l"],
+  "m": ["m"],
+  "n": ["n"],
+  "o": ["o"],
+  "p": ["p"],
+  "q": ["q"],
+  "r": ["r"],
+  "s": ["s"],
+  "t": ["t"],
+  "u": ["u"],
+  "v": ["v"],
+  "w": ["w"],
+  "x": ["x"],
+  "y": ["y"],
+  "z": ["z"],
+  " ": [" "]
+}
+
+function transformNormalText(text) {
+  if (text == "") {
+    return;
+  }
+
+  return text
+    .split('')
+    .map((c) => {
+      let possibleCharacters = charMap[c];
+      return possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)];
+    })
+    .join('');
+}
+
+$: {
+  console.log("[reload]")
+  veryNormalText = transformNormalText(veryNormalTextRaw);
+}
+</script>
+
+<input placeholder="Input some very normal text" bind:value={veryNormalTextRaw} />
+
+{veryNormalText}
