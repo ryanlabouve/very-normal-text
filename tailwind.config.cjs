caaa366fc2cef15adeb7f08358f48132db773f23
Ryan LaBouve <imryanlabouve@gmail.com>
2021-08-25 21:55:55 -0500

    Installs tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+const production = !process.env.ROLLUP_WATCH;
+
+module.exports = {
+  purge: {
+    content: [
+      "./src/**/*.svelte",
+    ],
+    enabled: production // disable purge in dev
+  },
+  darkMode: "media", // false or 'media' or 'class':: https://tailwindcss.com/docs/dark-mode
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
